750ca558e1755fb18ff5943424ab2a0b7fabf569
Господин <rdogdurbaev1@gmail.com>
2022-11-17 20:33:42 +0600

    first commit

---
+++ dart_hm.dart
@@ -0,0 +1,62 @@
+void main() {
+  int a = 10;
+  int b = 10;
+
+  print(a + b);
+
+  String name = 'Radmir';
+  String name1 = "Radmir";
+
+  print(name);
+  print(name1);
+
+  bool isValid = true;
+  print(isValid);
+
+  bool isCar = false;
+  print(isCar);
+
+  List array = ['car', 123, a, 'rqwd', 'qweqwe'];
+  print(array);
+
+  Map ra = {
+    "radmir": "radmir",
+    'age': 18,
+    'isMan': true,
+  };
+
+  print(ra);
+  print(ra['age']);
+
+//Finals
+  final a1 = "q";
+  final b1 = "w";
+  final c1 = "e";
+  final d1 = "r";
+  final e1 = "t";
+  final f1 = "y";
+
+//const
+  const q1 = "a";
+  const w1 = "b";
+  const e2 = "c";
+  const r1 = "d";
+  const t1 = "e";
+  const y1 = "f";
+
+//dynamic
+  dynamic a5 = {
+    "rwqeqw": "qeqweqew",
+  };
+  dynamic a6 = 123;
+  dynamic a7 = "qwerty";
+  dynamic a8 = 10.2;
+  dynamic a9 = [123123, 1231231, 1231321];
+
+//vars
+  var rre = "qwerty";
+  var eren = 'qwerty';
+  var rem = 'AWDADW';
+  var subaru = 'wadqw';
+  var ranobe = "dawawd";
+}
